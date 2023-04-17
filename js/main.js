1033fc12df1cc08aef9ac0ab0c7bcfaa46c054a3
YossefMagdy <121129964+YossefMagdy@users.noreply.github.com>
2023-04-17 02:25:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function copy(x){
     let id=x.parentNode.getAttribute("id")
     let range=document.createRange();
     range.selectNode(document.getElementById(id))
+      window.getSelection().removeAllRanges();
     window.getSelection().addRange(range); // to select text
     document.execCommand("copy");
     window.getSelection().removeAllRanges();// to deselect
