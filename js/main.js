7c64e1ec833b1776759a7aea3321df613d0ca4a0
YossefMagdy <y.f.magdy@gmail.com>
2023-04-17 02:20:09 +0200

    Upload Solved task

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function copy(x){
+  
+    x.children[0].classList.toggle('fa-check')
+
+    let id=x.parentNode.getAttribute("id")
+    let range=document.createRange();
+    range.selectNode(document.getElementById(id))
+    window.getSelection().addRange(range); // to select text
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();// to deselect
+}
+
